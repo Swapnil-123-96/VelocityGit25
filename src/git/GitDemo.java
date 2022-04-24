ebbb1baebca252af664bd6ea18ccd2b7ef9d3de4
swapnil <bakkewarswapnil@gmail.com>
2022-04-24 18:47:47 +0530

    rtn

--- src/git/GitDemo.java
+++ src/git/GitDemo.java
@@ -2,6 +2,6 @@ package git;
 
 public class GitDemo {
 public static void main(String[] args) {
-	System.out.println("My 1st Git project hello kgf jkkjkjk");
+	System.out.println("My 1st Git project hello kgf hhjkjnnn");
 }
 }
